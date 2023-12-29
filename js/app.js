b0e86b5480892b1924395f834fe50790d9f9c458
Mohammad Kiaei <78914731+mohammadkiaei@users.noreply.github.com>
2023-12-29 17:58:42 +0330

    initial commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const icon = document.querySelector('.icon');
+const menu = document.querySelector('.menu');
+
+icon.addEventListener('click', openMenu);
+menu.addEventListener('click', closeMenu);
+
+function openMenu() {
+   menu.classList.remove('hide');
+   icon.src = 'images/icon-close.svg';
+}
+function closeMenu() {
+   menu.classList.add('hide');
+   icon.src = 'images/icon-hamburger.svg';
+}
+
+// Developed By Mohammad Kiaei
